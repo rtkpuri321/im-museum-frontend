21cf87c2afb41ed5fcef2b19eeff2880f8ae0eee
rtkpuri321 <rtkpapuri321@gmail.com>
2024-05-26 15:22:40 +0530

    jwt token validation changes

--- im-museum-ui/src/FetchWithToken.js
+++ im-museum-ui/src/FetchWithToken.js
@@ -1,9 +1,34 @@
+import { jwtDecode } from 'jwt-decode';
+
 function fetchWithToken(url, options) {
     // Retrieve the access_token from cookies
     const cookies = document.cookie.split(';').map(cookie => cookie.split('='));
     const accessTokenCookie = cookies.find(cookie => cookie[0].trim() === 'access_token');
     const accessToken = accessTokenCookie ? accessTokenCookie[1] : null;
   
+    // If the access token is missing, redirect to login
+    if (!accessToken) {
+      window.location.href = '/';
+      return;
+    }
+
+    // Decode the token payload
+    let decodedToken;
+    try {
+      decodedToken = jwtDecode(accessToken);
+    } catch (error) {
+      console.error('Invalid token:', error);
+      window.location.href = '/';
+      return;
+    }
+
+    // Check if the token is expired
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (decodedToken.exp < currentTime) {
+      window.location.href = '/';
+      return;
+    }
+
     // Include the access_token in the headers if available
     if (accessToken) {
       options.headers = {
@@ -14,7 +39,7 @@ function fetchWithToken(url, options) {
   
     // Perform the fetch request with the modified options
     return fetch(url, options);
-  }
+}
 
 
 export default fetchWithToken;
